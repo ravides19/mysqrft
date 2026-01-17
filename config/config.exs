6519a2f599e768d76b1ded688eeb456813795432
Ravi Des <ravi1917d@gmail.com>
2026-01-17 11:51:27 +0530

    MySqrft auth basic generated

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :my_sqrft, :scopes,
+  user: [
+    default: true,
+    module: MySqrft.Auth.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: MySqrft.AuthFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :my_sqrft,
   ecto_repos: [MySqrft.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
--- lib/my_sqrft_web/router.ex
+++ lib/my_sqrft_web/router.ex
@@ -1,5 +1,7 @@
 defmodule MySqrftWeb.Router do
   use MySqrftWeb, :router
+
+  import MySqrftWeb.UserAuth
   import PhoenixStorybook.Router
 
   scope "/" do
@@ -13,6 +15,7 @@ defmodule MySqrftWeb.Router do
     plug :put_root_layout, html: {MySqrftWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -46,5 +49,33 @@ defmodule MySqrftWeb.Router do
       live_dashboard "/dashboard", metrics: MySqrftWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", MySqrftWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MySqrftWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", MySqrftWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{MySqrftWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
